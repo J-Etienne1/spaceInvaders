3aefbcfaf1b23d0f7cf96666da403ad3624f3ce9
Jason <jasonetienne1@gmail.com>
2023-08-19 14:49:57 +0100

    added player movment within screen size

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class Player {
     // test setting an image on the screen
     // c.fillStyle = 'red'
     // c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    if (this.image)
+    //if (this.position)
       c.drawImage(
         this.image,
         this.position.x,
@@ -39,24 +39,84 @@ class Player {
         this.height
       );
   }
+  update() {
+    if (this.image) {
+      this.draw();
+      this.position.x += this.velocity.x;
+    }
+  }
 }
 
 const player = new Player();
-player.draw();
+//player.draw();
+const keys = {
+    ArrowLeft:{
+        pressed: false
+    },
+    ArrowRight:{
+        pressed: false
+    },
+    space:{
+        pressed: false
+    }
+}
 
 // image need to load to need animate to keep calling our images
 function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
-  player.draw();
+  player.update();
+
+  if (keys.ArrowLeft.pressed && player.position.x >= 0){
+    player.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed  && player.position.x + player.width <=  canvas.width) {
+    player.velocity.x = 5;
+  }  else {
+    player.velocity.x = 0;
+  }
 }
 
 animate();
 
+addEventListener("keydown", ({ key }) => {
+  // check what keys we need
+  //console.log(key)
+  switch (key) {
+    case "ArrowLeft":
+      //console.log("left");
+      
+      keys.ArrowLeft.pressed = true
+      break;
+    case "ArrowRight":
+      //console.log("right");
+      keys.ArrowRight.pressed = true
+      break;
+    case " ":
+      //console.log("space");
+      keys.space.pressed = true
+      break;
+  }
+});
 
 
 
-addEventListener('keydown', (event)=>{
-    console.log(event)
-})
+addEventListener("keyup", ({ key }) => {
+    // check what keys we need
+    //console.log(key)
+    switch (key) {
+      case "ArrowLeft":
+        //console.log("left");
+        
+        keys.ArrowLeft.pressed = false
+        break;
+      case "ArrowRight":
+        //console.log("right");
+        keys.ArrowRight.pressed = false
+        break;
+      case " ":
+        //console.log("space");
+        keys.space.pressed = false
+        break;
+    }
+  });
