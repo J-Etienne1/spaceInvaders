7685ed760ed4c73f6140ee212e1f931aac1a97b7
Jason <jasonetienne1@gmail.com>
2023-08-21 17:58:42 +0100

    added ionvadder projectiles

--- index.js
+++ index.js
@@ -101,6 +101,18 @@ document.addEventListener("DOMContentLoaded", function () {
         this.position.y += velocity.y;
       }
     }
+    shoot(InvaderProjectiles){
+      InvaderProjectiles.push(new InvaderProjectile({
+        position:{
+          x: this.position.x + this.width / 2,
+          y: this.position.y + this.height
+        },
+        velocity:{
+          x:0,
+          y:5
+        }
+      }))
+    }
   }
 
   class Grid {
@@ -168,10 +180,34 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+
+
+  class InvaderProjectile {
+    constructor({ position, velocity }) {
+      (this.position = position), (this.velocity = velocity), (this.width = 4), (this.height = 10);
+
+
+    }
+    draw() {
+      c.fillStyle = 'white'
+     c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+    }
+  }
+
+
+
+
   // Render Player /Invder /Projectile
   const player = new Player();
   const projectiles = [];
   const grids = [];
+  const InvaderProjectiles = [];
   //player.draw();
   const keys = {
     ArrowLeft: {
@@ -185,8 +221,8 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   };
 
-  let frames = 0;
 
+  let frames = 0;
   let randomInterval = Math.floor(Math.random() * 500) + 500;
   // console.log(randomInterval)
 
@@ -196,6 +232,24 @@ document.addEventListener("DOMContentLoaded", function () {
     c.fillStyle = "black";
     c.fillRect(0, 0, canvas.width, canvas.height);
     player.update();
+    InvaderProjectiles.forEach((InvaderProjectile, index) =>{
+      if (InvaderProjectile.position.y + InvaderProjectile.height >= canvas.height){
+        setTimeout(() => {
+          InvaderProjectiles.splice(index, 1);
+        }, 0);
+      }
+      InvaderProjectile.update()
+
+      if (InvaderProjectile.position.y + InvaderProjectile.height >= player.position.y && InvaderProjectile.position.x + InvaderProjectile.width >= player.position.x && InvaderProjectile.position.x <= player.position.x + player.width){
+        console.log("YOusdfuhosfdg")
+      }
+
+    })
+
+
+    // console.log(InvaderProjectiles)
+
+
     projectiles.forEach((projectile, index) => {
       if (projectile.position.y + projectile.radius <= 0) {
         setTimeout(() => {
@@ -206,8 +260,12 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-    grids.forEach((grid) => {
+    grids.forEach((grid,gridIndex ) => {
       grid.update();
+      // spawn projectiles
+    if (frames % 100 === 0 && grid.invaders.length >0){
+      grid.invaders[Math.floor(Math.random() * grid.invaders.length)].shoot(InvaderProjectiles)
+    }
       grid.invaders.forEach((invader, i) => {
         invader.update({ velocity: grid.velocity });
 
@@ -226,12 +284,25 @@ document.addEventListener("DOMContentLoaded", function () {
               );
 
               const projectileFound = projectiles.find(
-                (projectile2) => projectile2 === projectile
+                (projectile2) => projectile2 === projectile 
               );
 
               if (invaderFound && projectileFound) {
                 grid.invaders.splice(i, 1);
                 projectiles.splice(j, 1);
+
+                if (grid.invaders.length > 0) {
+                  const firstInvader = grid.invaders[0];
+                  const lastInvader = grid.invaders[grid.invaders.length - 1];
+
+                  grid.width =
+                    lastInvader.posotion.x -
+                    firstInvader.position.x +
+                    lastInvader.width;
+                    grid.position.x = firstInvader.position.x
+                } else {
+                  grids.splice(gridIndex, 1)
+                }
               }
             }, 0);
           }
@@ -262,6 +333,9 @@ document.addEventListener("DOMContentLoaded", function () {
       frames = 0;
       //console.log(randomInterval)
     }
+
+
+    
 
     frames++;
   }
